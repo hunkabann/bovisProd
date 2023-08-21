185f4ab8b0f7fa63b885678ebd66b9533d1ed4bc
hunkabann <36930991+hunkabann@users.noreply.github.com>
2023-08-21 01:27:46 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   urlApiBovis: 'https://web-api-bovis.azurewebsites.net/',
   //redirectUri: 'https://proud-sea-0d98bd40f.2.azurestaticapps.net/',
   redirectUri: 'https://witty-pebble-0757dbf10.3.azurestaticapps.net',
-  clientID: '087f3ba6-2417-4f9d-a6ce-378095681e11',  
+  //clientID: '087f3ba6-2417-4f9d-a6ce-378095681e11',  
+  clientID: 'e2f9dd41-14a0-4920-8d13-27574aa0a1cf',  
   urlAuthority: 'https://login.microsoftonline.com/49ef9ec4-2b0b-47a1-94f3-e82844c4f61f'
 };
