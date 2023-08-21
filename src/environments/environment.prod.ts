b68b5613e3bdcdf1e1a557e7bd86d939635fb3ed
hunkabann <36930991+hunkabann@users.noreply.github.com>
2023-08-21 12:02:36 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   redirectUri: 'https://witty-pebble-0757dbf10.3.azurestaticapps.net',
   //clientID: '087f3ba6-2417-4f9d-a6ce-378095681e11',  
   //clientID: 'e2f9dd41-14a0-4920-8d13-27574aa0a1cf',  
-  clientID: '250372f4-4e4d-40f4-9231-1ba06c4f8b18',
+  //clientID: '250372f4-4e4d-40f4-9231-1ba06c4f8b18',
   clientID: '6b68c6c5-f257-472b-ade0-21c55d711424',
   urlAuthority: 'https://login.microsoftonline.com/49ef9ec4-2b0b-47a1-94f3-e82844c4f61f'
 };
