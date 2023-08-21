23a8c94df6afbb90ac849550800aa843defbb37b
Sebastian Flores <sfa120997@gmail.com>
2023-08-21 00:23:06 -0600

    Adjustment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   urlApiBovis: 'https://web-api-bovis.azurewebsites.net/',
-  redirectUri: 'https://proud-sea-0d98bd40f.2.azurestaticapps.net/',
+  //redirectUri: 'https://proud-sea-0d98bd40f.2.azurestaticapps.net/',
+  redirectUri: 'https://witty-pebble-0757dbf10.3.azurestaticapps.net',
   clientID: '087f3ba6-2417-4f9d-a6ce-378095681e11',  
   urlAuthority: 'https://login.microsoftonline.com/49ef9ec4-2b0b-47a1-94f3-e82844c4f61f'
 };
